a57c9be0ae1cc788b4660a361a5437eeaa650755
Somogyvári Márton Péter (SZF_2022_2024) <somogyvari.marton.peter@diak.szamalk-szalezi.hu>
2024-04-12 13:47:11 +0200

    first commit

---
+++ backend/alap_breeze/app/Models/Recept.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recept extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nev',
+        'kat_id',
+        'kep_eleresi_ut',
+        'leiras',
+    ];
+
+}
